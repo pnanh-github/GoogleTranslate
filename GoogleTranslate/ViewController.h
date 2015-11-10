e975294bb345d32118e258eb443c18eb7771cf22
Frank Pham <pnanh@me.com>
2015-11-10 14:29:31 +0700

    Initial Commit

---
+++ GoogleTranslate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleTranslate
+//
+//  Created by ADMIN on 11/10/15.
+//  Copyright © 2015 Nhat Tung Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoogleTranslate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoogleTranslate
+//
+//  Created by ADMIN on 11/10/15.
+//  Copyright © 2015 Nhat Tung Media. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
