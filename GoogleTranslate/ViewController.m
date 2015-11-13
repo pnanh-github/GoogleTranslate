856fb15b120f5783a477e575fb651241cad02ef2
Frank Pham <pnanh@me.com>
2015-11-13 10:47:11 +0700

    version 1.0

--- GoogleTranslate/ViewController.m
+++ GoogleTranslate/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/NSJSONSerialization.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *txtEng;
+- (IBAction)Translate:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *txtVN;
 
 @end
 
@@ -24,4 +28,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Translate:(id)sender {
+    NSString *apiKEY = [NSString stringWithContentsOfURL:[NSURL URLWithString:@"http://pnanh.azurewebsites.net/translate_api.txt"] encoding:NSUTF8StringEncoding error:nil];
+    NSString *data = [self.txtEng.text stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+    NSURL *url =[NSURL URLWithString:[NSString stringWithFormat:@"https://translate.yandex.net/api/v1.5/tr.json/translate?key=%@&text=%@&lang=en-vi&format=plain",apiKEY,data]];
+    NSError *error=nil;
+    //NSString *string = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+    if(error!=nil){
+        NSLog(@"Error: %@",error);
+    }
+    //NSLog(@"%@",string);
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] options:0 error:nil];
+    NSString *string = [[dict objectForKey:@"text"] objectAtIndex:0];
+    NSLog(@"%@",string);
+    self.txtVN.text = string;
+}
 @end
